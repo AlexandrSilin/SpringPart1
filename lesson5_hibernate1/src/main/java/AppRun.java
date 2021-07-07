813ea1a9e832deee89b921d9df5b012e0fe1674e
AlexandrSilin <67175@mail.ru>
2021-07-07 11:54:13 +0300

    work on bugs

--- lesson5_hibernate1/src/main/java/AppRun.java
+++ lesson5_hibernate1/src/main/java/AppRun.java
@@ -1,26 +1,22 @@
-import entities.Product;
 import org.hibernate.cfg.Configuration;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 public class AppRun {
     public static void main(String[] args) {
         EntityManagerFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-        EntityManager manager = factory.createEntityManager();
-        ProductDao productDao = new ProductDao(manager);
+        ProductDao productDao = new ProductDao(factory);
 //        manager.getTransaction().begin();
 //        for (int i = 0; i < 5; i++) {
 //            manager.persist(new Product(null, "product" + i, (int)(1 + Math.random() * Short.MAX_VALUE)));
 //        }
 //        manager.getTransaction().commit();
-        System.out.println(productDao.productFindById(1L));
-        productDao.saveOrUpdate(new Product(1L, "product013", 11098));
-        System.out.println(productDao.productFindById(1L));
-        productDao.deleteById(1L);
-        System.out.println(productDao.productFindById(1L));
-        System.out.println(productDao.findAll());
-        productDao.close();
-        manager.close();
+//        System.out.println(productDao.productFindById(5L));
+//        productDao.save(new Product(55L, "product0135", 161098));
+//        productDao.save(new Product(5L, "product013", 11098));
+//        System.out.println(productDao.productFindById(5L));
+        productDao.deleteById(2L);
+        System.out.println(productDao.productFindById(2L));
+//        System.out.println(productDao.findAll());
     }
 }
--- lesson5_hibernate1/src/main/java/ProductDao.java
+++ lesson5_hibernate1/src/main/java/ProductDao.java
@@ -1,41 +1,74 @@
 import entities.Product;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class ProductDao {
-    private final EntityManager entityManager;
+    private final EntityManagerFactory factory;
 
-    public ProductDao(EntityManager entityManager) {
-        this.entityManager = entityManager;
+    public ProductDao(EntityManagerFactory factory) {
+        this.factory = factory;
     }
 
     public Product productFindById(Long id) {
-        return entityManager.find(Product.class, id);
-    }
-
-    public List<Product> findAll() {
-        return entityManager.createQuery("select p from Product p ", Product.class).getResultList();
-    }
-
-    public void deleteById(Long id) {
-        entityManager.getTransaction().begin();
         try {
-            entityManager.remove(productFindById(id));
-            entityManager.getTransaction().commit();
-        } catch (IllegalArgumentException e) {
+            return executeForEntityManager(manager ->
+                    (manager.createQuery("select p from Product p where p.id = :id", Product.class)
+                            .setParameter("id", id)).getSingleResult());
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-            entityManager.getTransaction().rollback();
+            return null;
         }
     }
 
-    public void saveOrUpdate(Product product) {
-        entityManager.getTransaction().begin();
-        entityManager.merge(product);
-        entityManager.getTransaction().commit();
+    public List<Product> findAll() {
+        return executeForEntityManager(manager -> manager.createQuery("select p from Product p", Product.class).getResultList());
     }
 
-    public void close() {
-        entityManager.close();
+    public void deleteById(Long id) {
+        executeInTransaction(manager -> manager.createQuery("delete from Product p where p.id = :id")
+                .setParameter("id", id).executeUpdate());
+    }
+
+    private void update(Product product) {
+        executeInTransaction(manager -> manager.merge(product));
+    }
+
+    private void insert(Product product) {
+        executeInTransaction(manager -> manager.persist(product));
+    }
+
+    public void save(Product product) {
+        if (product.getId() == null) {
+            insert(product);
+        } else {
+            update(product);
+        }
+    }
+
+    private <R> R executeForEntityManager(Function<EntityManager, R> function) {
+        EntityManager manager = factory.createEntityManager();
+        try {
+            return function.apply(manager);
+        } finally {
+            manager.close();
+        }
+    }
+
+    private void executeInTransaction(Consumer<EntityManager> managerConsumer) {
+        EntityManager manager = factory.createEntityManager();
+        try {
+            manager.getTransaction().begin();
+            managerConsumer.accept(manager);
+            manager.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            manager.getTransaction().rollback();
+        } finally {
+            manager.close();
+        }
     }
 }
